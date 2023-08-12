d3565ca59d0456356c841852b7a3d7887fbd2dec
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2023-08-12 20:07:15 +0530

    Now only Valid Information is displayed

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../App'
 
 const CountryInfo = () => {
     const { country, select } = useContext(AppContext);
+    const [inValid] = useState(["gini", "tld", "cca2", "ccn3", "cca3", "cioc", "latlng", "landlocked", "demonyms", "maps", "idd", "capitalInfo", "translations", "coatOfArms", "currencies"]);
+
     return (
         <div className="country">
             <div className="header">
@@ -12,8 +14,8 @@ const CountryInfo = () => {
             <div className="flag" style={{ backgroundImage: `url(${country.flags.png})` }}></div>
             <div className="wrapper">
                 {Object.entries(country).map(([key, value]) => {
-                    return (
-                        <div className="data" key={key}>
+                    return !inValid.includes(key) &&
+                        (<div className="data" key={key}>
                             <div className="info">{key}</div>
                             <div className="detail">
                                 {Array.isArray(value) && value.map(value => <span key={value}>{value}</span>)}
